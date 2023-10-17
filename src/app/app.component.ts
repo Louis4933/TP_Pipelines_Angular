d8a4906e0e417056ceadb794de03fbb1263f416b
louis <floreanilo@cy-tech.fr>
2023-10-17 14:41:32 +0200

    Initial Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'TP_Pipelines_Angular';
+  tmp = "test linter et build and deploy 2";
+}
